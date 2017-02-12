5e211b88413eb36e4472843d571928f644e18f31
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-02-11 23:24:40 -0500

    remade loop and description into m5 m7

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -8,65 +8,46 @@ class ProjectsSection extends Component {
     return (
           <div className="projects">
           <a name="projects"></a>
-            <Row className="no-margin">
-              <h1 id="projects-title">PROJECTS / TALKS</h1>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                  <div className="statement">
-                    <h2>Loop</h2>
-                    <div className="video-container">
-                      <iframe width="500" height="281" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
-                    </div>
-                    <p>
-                      <span>
-                        A tool for giving teachers realtime feedback from students in the classroom.
-                        Deployed at: <a href="https://loop-teach.herokuapp.com">loop-teach.herokuapp.com</a>
-                      </span>
-                    </p>
+            <Row>
+              <Col xs={12} sm={12} md={5} lg={5} className="description">
+                <div>
+                  <h2>Loop</h2>
+                  <p>
+                    <span>
+                      Loop is a communication tool that allows teachers to check for understanding while class is in session.
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                      Loop provides teachers with feedback from students in realtime, using Sockets.io, React to quickly handle the state of mutiple students, and D3, to visualise classroom mood.
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                        Loop is deployed at: <a href="https://loop-teach.herokuapp.com">loop-teach.herokuapp.com</a>
+                    </span>
+                  </p>
+                </div>
+              </Col>
+              <Col xs={12} sm={12} md={7} lg={7} className="media">
+                <div>
+                  <div className="video-container">
+                    <iframe width="500" height="281" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
                   </div>
-                </Col>
-
-                <Col xs={12} sm={12} md={6} lg={3}>
-                  <div className="statement">
-                    <h2>Tails</h2>
-                    <div className="video-container">
-                      <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
-                    </div>
-                    <p>
-                      <span>
-                      A brief talk about remaining anonymous online using TAIL, and the TOR network.
-                      </span>
-                    </p>
-                  </div>
-                </Col>
-
-                <Col xs={12} sm={12} md={6} lg={3}>
-                  <div className="statement">
-                    <h2>Cube Master</h2>
-                    <div className="video-container">
-                      <iframe width="500" height="281" src="https://www.youtube.com/embed/PNkrEoNgo-A" frameBorder="0" allowFullScreen></iframe>
-                    </div>
-                    <p>
-                      <span>
-                      Unitys "Roll A Ball" tutorial remade in Virtual Reality.
-                      </span>
-                    </p>
-                  </div>
-                </Col>
-
-                <Col xs={12} sm={12} md={6} lg={3}>
-                  <div className="statement">
-                    <h2>Home Cooked</h2>
-                    <div className="video-container">
-                      <iframe width="500" height="281" src="https://www.youtube.com/embed/aQAHGaJZjM8?list=PLx0iOsdUOUmk4F-mEQuJsLn41qXF5alF9" frameBorder="0" allowFullScreen></iframe>
-                    </div>
-                    <p>
-                      <span>
-                      A basic receipe site, using Yummlys API to match search querys with ingredients.
-                      </span>
-                    </p>
-                  </div>
-                </Col>
-
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} sm={12} md={7} lg={7} className="description">
+                <div>
+                  <h1>video</h1>
+                </div>
+              </Col>
+              <Col xs={12} sm={12} md={5} lg={5} className="media">
+                <div>
+                  <h1>description</h1>
+                </div>
+              </Col>
             </Row>
           </div>
     );
