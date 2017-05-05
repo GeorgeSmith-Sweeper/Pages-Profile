d791b82b0b2f2821d2c02eb4a6ef0c5c1b5288e1
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-04 21:01:02 -0400

    moved the home cooked below loop

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -68,6 +68,43 @@ class ProjectsSection extends Component {
                 </div>
               </Col>
             </Row>
+            <Row className="projectsRows">
+              <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
+                <div className="description">
+                  <h3>Home Cooked</h3>
+                  <p>
+                    <span>
+                    Ever have trouble making something to eat with the ingredients you have in the fridge? I did too, so I made this site to help me create delicious meals.
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                     Home Cooked allows you to enter ingredients into a search bar and only get back recipes that contain those components.
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                      Who knew dinner could be solved with software.
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                     I built this project using Yummly's API, Vanilla JS, and a dash of Bootstrap for the Jumbotron.
+                    </span>
+                  </p>
+                  <p>
+                    <span>
+                     Home Cooked is deployed <a href="http://fridge-project.surge.sh/" className="project-links">here</a>.
+                    </span>
+                  </p>
+                </div>
+              </Col>
+              <Col xs={12} sm={12} md={7} lg={7} className="media">
+                <div className="video-container">
+                  <iframe width="500" height="281" src="https://www.youtube.com/embed/aQAHGaJZjM8?list=PLx0iOsdUOUmk4F-mEQuJsLn41qXF5alF9" frameBorder="0" allowFullScreen></iframe>
+                </div>
+              </Col>
+            </Row>
              {/* Next Project */}
             <Row className="projectsRows">
               <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
@@ -124,44 +161,6 @@ class ProjectsSection extends Component {
               <Col xs={12} sm={12} md={7} lg={7} className="media">
                 <div className="video-container">
                   <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
-                </div>
-              </Col>
-            </Row>
-            {/* Next Project */}
-            <Row className="projectsRows">
-              <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
-                <div className="description">
-                  <h3>Home Cooked</h3>
-                  <p>
-                    <span>
-                    Ever have trouble making something to eat with the ingredients you have in the fridge? I did too, so I made this site to help me create delicious meals.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                     Home Cooked allows you to enter ingredients into a search bar and only get back recipes that contain those components.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                      Who knew dinner could be solved with software.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                     I built this project using Yummly's API, Vanilla JS, and a dash of Bootstrap for the Jumbotron.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                     Home Cooked is deployed <a href="http://fridge-project.surge.sh/" className="project-links">here</a>.
-                    </span>
-                  </p>
-                </div>
-              </Col>
-              <Col xs={12} sm={12} md={7} lg={7} className="media">
-                <div className="video-container">
-                  <iframe width="500" height="281" src="https://www.youtube.com/embed/aQAHGaJZjM8?list=PLx0iOsdUOUmk4F-mEQuJsLn41qXF5alF9" frameBorder="0" allowFullScreen></iframe>
                 </div>
               </Col>
             </Row>
