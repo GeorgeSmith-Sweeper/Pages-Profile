805b902c86d67399c4d795a29f25128c64efef48
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-06 09:17:18 -0400

    completed Tails component, cleaned projects section

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
 import ColorStream from './projects/ColorStream';
 import Loop from './projects/Loop';
 import HomeCooked from './projects/HomeCooked';
 import CubeMaster from './projects/CubeMaster';
+import Tails from './projects/Tails';
 
 class ProjectsSection extends Component {
 
@@ -11,40 +11,12 @@ class ProjectsSection extends Component {
     return (
         <div className="projects">
           <a name="projects"></a>
-            <ColorStream />
-            <Loop />
-            <HomeCooked />
-            <CubeMaster />
-
-            <Row className="projectsRows">
-              <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
-                <div className="description">
-                  <h3>Tails</h3>
-                  <p>
-                    <span>
-                      Given the revelations about the NSA’s massive surveillance operations, and large scale data collection done by advertisers, I thought it would be appropriate to give
-                      a brief talk about remaining anonymous online.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                      This talk covers the Tails OS, and skims the surface of how the TOR network handles communications, and uses public and private keys to encrypt information.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                      If you enjoyed this talk, more information can be found <a href="https://tails.boum.org/" className="project-links">here</a>.
-                    </span>
-                  </p>
-                </div>
-              </Col>
-              <Col xs={12} sm={12} md={7} lg={7} className="media">
-                <div className="video-container">
-                  <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
-                </div>
-              </Col>
-            </Row>
-          </div>
+          <ColorStream />
+          <Loop />
+          <HomeCooked />
+          <CubeMaster />
+          <Tails />
+        </div>
     );
   }
 }
