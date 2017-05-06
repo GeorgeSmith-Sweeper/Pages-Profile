f955e82aee7503f40911ae4996a39d2f4cb1e67c
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-05 23:47:13 -0400

    setup HomeCooked component, added imports

--- src/projects/CubeMaster.js
+++ src/projects/CubeMaster.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 
 class CubeMaster extends Component {
---
+++ src/projects/HomeCooked.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+
+class HomeCooked extends Component {
+  render () {
+    return (
+
+
+    )
+  }
+}
+
+
+export default HomeCooked;
