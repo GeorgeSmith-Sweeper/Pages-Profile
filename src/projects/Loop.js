c5b393dc05ed25866d799a1bedc741b495e08836
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-05 23:50:30 -0400

    added loop component

---
+++ src/projects/Loop.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+
+class Loop extends Component {
+  render () {
+    return (
+
+
+    )
+  }
+}
+
+
+export default Loop;
