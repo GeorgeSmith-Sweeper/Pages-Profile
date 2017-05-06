1b7c68874eca9c4ff8953a1dedf1573ebc9de8e1
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-05 23:40:09 -0400

    cleared unused Image var

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import ColorStream from './projects/ColorStream';
 
 
