4a0282172d0768087f8f8830d6bf06c9702d2228
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-06 09:06:13 -0400

    finished Loop component

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import ColorStream from './projects/ColorStream';
-
+import Loop from './projects/Loop';
 
 class ProjectsSection extends Component {
 
@@ -10,33 +10,7 @@ class ProjectsSection extends Component {
         <div className="projects">
           <a name="projects"></a>
             <ColorStream />
-            <Row className="projectsRows">
-              <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
-                <div className="description">
-                  <h3>Loop</h3>
-                  <p>
-                    <span>
-                      Loop is a communication tool that allows teachers to check for understanding while class is in session.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                      Loop provides teachers with feedback from students in realtime, using sockets.io and React to quickly handle the state of multiple students, and D3, to visualize classroom mood.
-                    </span>
-                  </p>
-                  <p>
-                    <span>
-                        Loop is deployed <a href="https://loop-teach.herokuapp.com" className="project-links">here</a>.
-                    </span>
-                  </p>
-                </div>
-              </Col>
-              <Col xs={12} sm={12} md={7} lg={7} className="media">
-                <div className="video-container">
-                  <iframe width="500" height="281" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
-                </div>
-              </Col>
-            </Row>
+            <Loop />
             <Row className="projectsRows">
               <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
                 <div className="description">
--- src/projects/Loop.js
+++ src/projects/Loop.js
@@ -3,10 +3,36 @@ import { Row, Col } from 'react-bootstrap';
 
 
 class Loop extends Component {
+
   render () {
     return (
-
-
+      <Row className="projectsRows">
+        <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
+          <div className="description">
+            <h3>Loop</h3>
+            <p>
+              <span>
+                Loop is a communication tool that allows teachers to check for understanding while class is in session.
+              </span>
+            </p>
+            <p>
+              <span>
+                Loop provides teachers with feedback from students in realtime, using sockets.io and React to quickly handle the state of multiple students, and D3, to visualize classroom mood.
+              </span>
+            </p>
+            <p>
+              <span>
+                  Loop is deployed <a href="https://loop-teach.herokuapp.com" className="project-links">here</a>.
+              </span>
+            </p>
+          </div>
+        </Col>
+        <Col xs={12} sm={12} md={7} lg={7} className="media">
+          <div className="video-container">
+            <iframe width="500" height="281" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
+          </div>
+        </Col>
+      </Row>
     )
   }
 }
