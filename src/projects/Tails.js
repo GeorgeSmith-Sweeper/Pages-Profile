51bcdf84d4e3c0e705c189e1b57c11f0ba590bca
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-05 23:51:38 -0400

    added Tails component

---
+++ src/projects/Tails.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+
+class Tails extends Component {
+  render () {
+    return (
+
+
+    )
+  }
+}
+
+
+export default Tails;
