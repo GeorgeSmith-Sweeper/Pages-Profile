939cbe1fd76f4cdfc109fbb140e5a92e61ff9597
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-05-07 20:37:37 -0400

    added alt tag for Tails

--- src/projects/Tails.js
+++ src/projects/Tails.js
@@ -29,7 +29,7 @@ class Tails extends Component {
         </Col>
         <Col xs={12} sm={12} md={7} lg={7} className="media">
           <div className="video-container">
-            <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
+            <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" alt="A presentation about web security" allowFullScreen></iframe>
           </div>
         </Col>
       </Row>
