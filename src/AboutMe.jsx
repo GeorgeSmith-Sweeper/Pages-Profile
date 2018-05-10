c20b492a11e7e79f2b15685d0da25008d39aec7e
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-05-10 17:19:16 -0400

    add blog link to about me section

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -15,7 +15,14 @@ class AboutMe extends Component {
             <h2>George Smith-Sweeper</h2>
             <p>I am a software engineer based out of NYC, who enjoys solving puzzles, and taking complex problems and explaining them in a way that is easy for anyone to grasp.</p>
             <p>When it comes to building projects, I'm language agnostic. I've created projects using, JavaScript, Python, and Java, and am currently learning Elixir.</p>
-            <p>I love learning and teaching, and will continue to grow as a developer by listening to the feedback from others and refining my talents. I use technology everyday, and it makes sense to become part of the driving force that moves things forward, by continuing to learn everyday. This site will continue to evolve as my skill set grows, and frameworks change.</p>
+            <p>I love learning and teaching, and will continue to grow as a developer by listening to the feedback from others and refining my talents. I use technology everyday, and it makes sense to become part of the driving force that moves things forward, by continuing to learn everyday.
+            </p>
+            <p>
+              Feel free to check out my blog for a detailed look at my thoughts on building software.
+            </p>
+            <p>
+              <a href="http://georgesmith-sweeper.github.io/">The Road Through Code</a>
+            </p>
           </div>
         </div>
       </div>
