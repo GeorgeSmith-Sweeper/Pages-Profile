267e8fd4103b405f0e31194bfbcfd199db23826b
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-05-10 13:11:18 -0400

    update about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -14,7 +14,7 @@ class AboutMe extends Component {
           <div className="col s12 m8 l8">
             <h2>George Smith-Sweeper</h2>
             <p>I am a software engineer based out of NYC, who enjoys solving puzzles, and taking complex problems and explaining them in a way that is easy for anyone to grasp.</p>
-            <p>Most of my projects have been built using JavaScript, React, Bootstrap, and CSS.</p>
+            <p>When it comes to building projects, I'm language agnostic. I've created projects using, JavaScript, Python, and Java, and am currently learning Elixir.</p>
             <p>I love learning and teaching, and will continue to grow as a developer by listening to the feedback from others and refining my talents. I use technology everyday, and it makes sense to become part of the driving force that moves things forward, by continuing to learn everyday. This site will continue to evolve as my skill set grows, and frameworks change.</p>
           </div>
         </div>
