a751460f3759b9b932a6399473975bfb059ee2d9
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-05-14 15:44:44 -0400

    convert Loop to pure function

--- src/projects/Tails.js
+++ src/projects/Tails.js
@@ -1,46 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 
-class Tails extends Component {
-  render () {
-    return (
-      <Row className="projectsRows">
-        <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
-          <div className="description">
-            <h3>Tails</h3>
-            <p>
-              <span>
-                Given the revelations about the NSA’s massive surveillance operations, and large scale data collection done by advertisers, I thought it would be appropriate to give
-                a brief talk about remaining anonymous online.
-              </span>
-            </p>
-            <p>
-              <span>
-                This talk covers the Tails OS, and skims the surface of how the TOR network handles communications, and uses public and private keys to encrypt information.
-              </span>
-            </p>
-            <p>
-              <span>
-                If you enjoyed this talk, more information can be found <a href="https://tails.boum.org/" className="project-links">here</a>.
-              </span>
-            </p>
-          </div>
-        </Col>
-        <Col xs={12} sm={12} md={7} lg={7} className="media">
-          <div className="video-container">
-            <iframe
-              width="500"
-              height="281"
-              src="https://www.youtube.com/embed/A-LKDSz4Ik4"
-              title="Tails Video"
-              frameBorder="0" alt="A presentation about web security" allowFullScreen></iframe>
-          </div>
-        </Col>
-      </Row>
-    )
-  }
-}
-
+const Tails = () => {
+  return (
+    <Row className="projectsRows">
+      <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
+        <div className="description">
+          <h3>Tails</h3>
+          <p>
+            <span>
+              Given the revelations about the NSA’s massive surveillance operations, and large scale data collection done by advertisers, I thought it would be appropriate to give
+              a brief talk about remaining anonymous online.
+            </span>
+          </p>
+          <p>
+            <span>
+              This talk covers the Tails OS, and skims the surface of how the TOR network handles communications, and uses public and private keys to encrypt information.
+            </span>
+          </p>
+          <p>
+            <span>
+              If you enjoyed this talk, more information can be found <a href="https://tails.boum.org/" className="project-links">here</a>.
+            </span>
+          </p>
+        </div>
+      </Col>
+      <Col xs={12} sm={12} md={7} lg={7} className="media">
+        <div className="video-container">
+          <iframe
+            width="500"
+            height="281"
+            src="https://www.youtube.com/embed/A-LKDSz4Ik4"
+            title="Tails Video"
+            frameBorder="0" alt="A presentation about web security" allowFullScreen>
+          </iframe>
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
 export default Tails;
