97fc3d174fcedc273109f57c553701f70f592450
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-05-17 17:41:55 -0400

    refactor landing header to pur function

--- src/LandingHeader.js
+++ src/LandingHeader.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
-class LandingHeader extends Component {
-  render () {
-    return (
-      <div className="landing">
-        <Row>
-          <Col sm={12}>
-            <div className="statement">
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+const LandingHeader = () => {
+  return (
+    <div className="landing">
+      <Row>
+        <Col sm={12}>
+          <div className="statement">
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default LandingHeader;
