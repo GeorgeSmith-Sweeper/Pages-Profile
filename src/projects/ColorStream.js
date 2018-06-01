c397f51a3917aac6b837f7981639e74c23fd23d7
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-06-01 18:59:40 -0400

    add article tags to all projects within section tag

--- src/projects/ColorStream.js
+++ src/projects/ColorStream.js
@@ -5,40 +5,42 @@ import Colorstream from '../resources/Colorstream.png';
 
 const ColorStream = () => {
   return (
-    <Row className="no-bottom-margin">
-      <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
-        <div className="description">
-          <h3>Color Stream</h3>
-          <p>
-            <span>
-              Color Stream uses JavaScript and CSS to generate vibrant gradient backgrounds. Each background is completely unique, and can be used as a great banner for your website or project.
-            </span>
-          </p>
-          <p>
-            <span>
-              Backgrounds can be recreated by clicking the "Reveal" button to display the exact CSS properties used to create the current gradient.
-            </span>
-          </p>
-          <p>
-            <span>
-              If you like this project, be sure to click the star button located on the page!
-            </span>
-          </p>
-          <p>
-            <span>
-                Color Stream is deployed <a href="http://colorstream.co" className="project-links">here</a>.
-            </span>
-          </p>
-        </div>
-      </Col>
-      <Col xs={12} sm={12} md={7} lg={7} className="media">
-        <div className="video-container">
-          <a href="http://colorstream.co">
-            <Image alt="colorstream's homepage" src={Colorstream} responsive />
-          </a>
-        </div>
-      </Col>
-    </Row>
+    <article>
+      <Row className="no-bottom-margin">
+        <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
+          <div className="description">
+            <h3>Color Stream</h3>
+            <p>
+              <span>
+                Color Stream uses JavaScript and CSS to generate vibrant gradient backgrounds. Each background is completely unique, and can be used as a great banner for your website or project.
+              </span>
+            </p>
+            <p>
+              <span>
+                Backgrounds can be recreated by clicking the "Reveal" button to display the exact CSS properties used to create the current gradient.
+              </span>
+            </p>
+            <p>
+              <span>
+                If you like this project, be sure to click the star button located on the page!
+              </span>
+            </p>
+            <p>
+              <span>
+                  Color Stream is deployed <a href="http://colorstream.co" className="project-links">here</a>.
+              </span>
+            </p>
+          </div>
+        </Col>
+        <Col xs={12} sm={12} md={7} lg={7} className="media">
+          <div className="video-container">
+            <a href="http://colorstream.co">
+              <Image alt="colorstream's homepage" src={Colorstream} responsive />
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </article>
   );
 };
 
--- src/projects/CubeMaster.js
+++ src/projects/CubeMaster.js
@@ -3,44 +3,46 @@ import { Row, Col } from 'react-bootstrap';
 
 const CubeMaster = () => {
   return (
-    <Row className="projectsRows">
-      <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
-        <div className="description">
-          <h3>Cube Master</h3>
-          <p>
-            <span>
-            Cube Master is the result of a short creative hackathon. I chose to remake Unity's "Roll A Ball" tutorial, while enhancing it to run on Samsung's Gear VR headset.
-            </span>
-          </p>
-          <p>
-            <span>
-            During the hackathon, I had to retrain myself to develop in three dimensions, and account for depth along with height and width.
-            </span>
-          </p>
-          <p>
-            <span>
-            Unity's game engine is incredibly powerful, and was a pleasure to work with.
-            </span>
-          </p>
-        </div>
-      </Col>
-      <Col xs={12} sm={12} md={7} lg={7} className="media">
-        <div>
-          <div className="video-container">
-            <iframe
-              width="500"
-              height="281"
-              src="https://www.youtube.com/embed/PNkrEoNgo-A"
-              title="Cube Mater Video"
-              frameBorder="0"
-              alt="A video presentation of Cube Master"
-              allowFullScreen
-            >
-            </iframe>
+    <article>
+      <Row className="projectsRows">
+        <Col xs={12} sm={12} md={5} lg={5} className="text-align-left">
+          <div className="description">
+            <h3>Cube Master</h3>
+            <p>
+              <span>
+              Cube Master is the result of a short creative hackathon. I chose to remake Unity's "Roll A Ball" tutorial, while enhancing it to run on Samsung's Gear VR headset.
+              </span>
+            </p>
+            <p>
+              <span>
+              During the hackathon, I had to retrain myself to develop in three dimensions, and account for depth along with height and width.
+              </span>
+            </p>
+            <p>
+              <span>
+              Unity's game engine is incredibly powerful, and was a pleasure to work with.
+              </span>
+            </p>
           </div>
-        </div>
-      </Col>
-    </Row>
+        </Col>
+        <Col xs={12} sm={12} md={7} lg={7} className="media">
+          <div>
+            <div className="video-container">
+              <iframe
+                width="500"
+                height="281"
+                src="https://www.youtube.com/embed/PNkrEoNgo-A"
+                title="Cube Mater Video"
+                frameBorder="0"
+                alt="A video presentation of Cube Master"
+                allowFullScreen
+              >
+              </iframe>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </article>
   );
 };
 
