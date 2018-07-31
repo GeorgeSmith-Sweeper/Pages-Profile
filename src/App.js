20d3a81b3fd4fc5e5716fa9c87245e7497627eba
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-07-31 10:21:08 -0400

    remove dividers between about me and projects

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LandingHeader from './LandingHeader';
 import ProjectsSection from './ProjectsSection';
 import AboutMe from './AboutMe';
 import ContactInfo from './ContactInfo';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <LandingHeader />
-        <div className="divider"></div>
-        <AboutMe />
-        <div className="divider"></div>
-        <ProjectsSection />
-        <ContactInfo />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <LandingHeader />
+      <AboutMe />
+      <ProjectsSection />
+      <ContactInfo />
+    </div>
+  );
+};
 
 export default App;
